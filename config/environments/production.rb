472f1d10d4e2346012878cc4d35dd108d52d9aa0
seiyamiyaoka <seiyamiyaoka@gmail.com>
2015-09-05 13:56:00 +0000

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.action_mailer.default_url_options = { host: 'morning-island-7489.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'shielded-atoll-7137.herokuapp.com' }
 end

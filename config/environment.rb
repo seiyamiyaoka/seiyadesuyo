5ecef7cd0379acb18bfe654cf64e0d8ca1e41e2f
seiyamiyaoka <seiyamiyaoka@gmail.com>
2015-10-18 07:23:38 +0000

    kyoudesu

--- config/environment.rb
+++ config/environment.rb
@@ -3,17 +3,17 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
-#  ActionMailer::Base.delivery_method = :smtp 
-#   ActionMailer::Base.smtp_settings =
+  ActionMailer::Base.delivery_method = :smtp 
+   ActionMailer::Base.smtp_settings =
   
-#  {
-#    :user_name => "app41119998@heroku.com", 
-#   :password => "2aagqgu62111", 
+  {
+    :user_name => "app41119998@heroku.com", 
+   :password => "2aagqgu62111", 
   
   
-#     :domain => "heroku.com", 
-#   :address => "smtp.sendgrid.net", 
-#   :port => 587, 
-#   :authentication => :plain, 
-#   :enable_starttls_auto => true 
-#  }
+     :domain => "heroku.com", 
+   :address => "smtp.sendgrid.net", 
+   :port => 587, 
+   :authentication => :plain, 
+   :enable_starttls_auto => true 
+  }

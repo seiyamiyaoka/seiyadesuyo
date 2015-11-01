c019be099c1370980a824057ffd6826ba418aed6
seiyamiyaoka <seiyamiyaoka@gmail.com>
2015-11-01 07:53:26 +0000

    gitflow

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -76,7 +76,7 @@ class BlogsController < ApplicationController
     end
 end
 
-  private
+      private
     # Use callbacks to share common setup or constraints between actions.
     def set_blog
       @blog = Blog.find(params[:id])
